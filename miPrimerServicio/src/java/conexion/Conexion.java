e6b73b27a6844b434b51dde72c6fdb5fe08bb878
Abel <li.carlos@upeu.edu.pe>
2020-06-09 02:52:32 -0500

    first commit

---
+++ miPrimerServicio/src/java/conexion/Conexion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexion {
+    
+    public Connection connectDataBase()throws ClassNotFoundException{
+    
+        Connection connection = null ;
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/sistemas","root","");
+        }catch(SQLException ex){
+            System.out.println("ERROR conexion.Conexion,connectDataBase()"+ex.getMessage());
+        
+        }
+        return connection;
+    }
+    
+}
